3ded9929fc279030a061cf34ea952191401c1619
ezechiel ben <ezechielben06@gmail.com>
2026-02-28 08:11:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9703229558-59574",
+  "appId": "1:618381400007:web:58b02b8aa5624dae506be2",
+  "apiKey": "AIzaSyBVKla8HOMDIvT_f_pNYopwvSXnN3qRGCM",
+  "authDomain": "studio-9703229558-59574.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "618381400007"
+};
